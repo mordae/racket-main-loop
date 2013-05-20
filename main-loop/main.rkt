d33fb67cf40d908a8d33a691bc660ef4c3628a9f
Jan Dvořák <mordae@anilinux.org>
2013-05-20 20:19:02 +0200

    protect watches with a semaphore
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- main-loop/main.rkt
+++ main-loop/main.rkt
@@ -21,63 +21,70 @@
   #:mutable)
 
 
+; Semaphore protecting watches, since hash table is a bit unsafe.
+(define/contract watches-semaphore semaphore?
+  (make-semaphore 1))
+
+; All registered watches.
+(define/contract watches (hash/c evt? watch?)
+  (make-hasheq))
+
+
 (define looping-thread
   (thread
     (thunk
       ; Event that signalizes this thread have some inbound messages.
       (define inbox-evt (thread-receive-evt))
 
-      ; All registered watches.
-      (define/contract watches (hash/c evt? watch?)
-        (make-hasheq `((,inbox-evt . ,(make-watch inbox-evt (set))))))
+      ; Subscribe to this event so that we can re-wait when watches change.
+      (hash-set! watches inbox-evt (make-watch inbox-evt (set)))
 
       ; Endlessly consume events as they come in.
       (for ((event (in-producer (thunk (apply sync (hash-keys watches))) #f)))
-
-        ; We need to process incoming messages first.
+        ; We need to process incoming messages that might have awoken us.
         (for ((message (in-producer thread-try-receive #f)))
-          (match message
-            ((list 'add-handler (var event) (var handler))
-             (let* ((watch (hash-ref! watches event
-                                      (thunk (make-watch event (set)))))
-                    (handlers (watch-handlers watch)))
-               (set-watch-handlers! watch (set-add handlers handler))))
-
-            ((list 'remove-handler (var event) (var handler))
-             (when (hash-has-key? watches event)
-               (let* ((watch (hash-ref watches event))
-                      (handlers (watch-handlers watch)))
-                 (set-watch-handlers! watch (set-remove handlers handler))
-                 (when (set-empty? (watch-handlers watch))
-                   (hash-remove! watches event)))))
-
-             ((list 'cancel (var event))
-              (when (hash-has-key? watches event)
-                (hash-remove! watches event)))))
+          (void message))
 
         ; Then we can try to process the event.
-        (when (hash-has-key? watches event)
-          (let ((watch (hash-ref watches event)))
-            (for ((handler (in-set (watch-handlers watch))))
-                 (with-handlers ((exn:fail? (lambda (exn)
-                                              ((error-display-handler)
-                                               (exn-message exn) exn))))
-                   (handler)))))))))
+        (for ((handler (in-set (call-with-semaphore watches-semaphore
+                                 (thunk
+                                   (let ((w (hash-ref watches event #f)))
+                                     (if w (watch-handlers w) (set))))))))
+          (with-handlers ((exn:fail? (lambda (exn) ((error-display-handler)
+                                                    (exn-message exn) exn))))
+            (handler)))))))
 
 
 (define/contract (add-event-handler event handler)
                  (-> evt? (-> void?) void?)
-  (thread-send looping-thread `(add-handler ,event ,handler)))
+  (call-with-semaphore watches-semaphore
+    (thunk
+      (let* ((watch (hash-ref! watches event (thunk (make-watch event (set)))))
+             (handlers (watch-handlers watch)))
+        (set-watch-handlers! watch (set-add handlers handler)))))
+  (thread-send looping-thread #t))
 
 
 (define/contract (remove-event-handler event handler)
                  (-> evt? (-> void?) void?)
-  (thread-send looping-thread `(remove-handler ,event ,handler)))
+  (call-with-semaphore watches-semaphore
+    (thunk
+      (when (hash-has-key? watches event)
+        (let* ((watch (hash-ref watches event))
+               (handlers (watch-handlers watch)))
+          (set-watch-handlers! watch (set-remove handlers handler))
+          (when (set-empty? (watch-handlers watch))
+            (hash-remove! watches event))))))
+  (thread-send looping-thread #t))
 
 
 (define/contract (cancel-event event)
                  (-> evt? void?)
-  (thread-send looping-thread `(cancel ,event)))
+  (call-with-semaphore watches-semaphore
+    (thunk
+      (when (hash-has-key? watches event)
+        (hash-remove! watches event))))
+  (thread-send looping-thread #t))
 
 
 (define/contract (call-later handler)
@@ -85,7 +92,7 @@
   (define (wrapper)
     (remove-event-handler always-evt wrapper)
     (handler))
-  (thread-send looping-thread `(add-handler ,always-evt ,wrapper)))
+  (add-event-handler always-evt wrapper))
 
 
 ; vim:set ts=2 sw=2 et:
